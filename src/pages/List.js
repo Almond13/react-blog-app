c14ef7b50aa4d749dd5e4a5c770f45e33802ae0e
mz_seungyeon <mz_seungyeon@woowafriends.com>
2024-06-28 16:29:12 +0900

    header 에러 해결후

--- src/pages/List.js
+++ src/pages/List.js
@@ -4,32 +4,24 @@ import {getPost} from "../api/getApi";
 import {useDispatch, useSelector} from "react-redux";
 import {detailActions, fetchListData} from "../redux/modules/detail";
 
-
 const List = () => {
-    const params = useParams()
-    const dispatch = useDispatch()
+    const params = useParams();
+    const dispatch = useDispatch();
 
-    const listData = useSelector((state) => state.detail.listData)
-    const listHeader = useSelector((state) => state.detail.listHeader)
-    const loading = useSelector((state) => state.detail.loading)
-    // const getListData =  () => {
-    //      dispatch(detailActions.getListData())
-    //     // console.log(listData)
-    // }
+    const listData = useSelector((state) => state.detail.listData);
+    const listHeader = useSelector((state) => state.detail.listHeader);
+    const loading = useSelector((state) => state.detail.loading);
 
     useEffect(() => {
-        dispatch(fetchListData())
-        // console.log(data)
-        // console.log(params)
-        console.log(listData)
-    }, [dispatch])
+        dispatch(fetchListData());
+    }, [dispatch]);
 
     if (loading) {
-        return <p>대기중</p>
+        return <p>대기중</p>;
     }
 
     if (listData.length === 0) {
-        return null
+        return null;
     }
 
     return (
@@ -37,7 +29,6 @@ const List = () => {
             <h1>어바웃</h1>
             <p>첫 번째 데이터의 ID: {listData[0].id}</p>
             <pre>{JSON.stringify(listHeader, null, 2)}</pre>
-            <button onClick={() => dispatch(fetchListData())}>데이터 다시 불러오기</button>
         </>
     );
 };
--- src/redux/modules/detail.js
+++ src/redux/modules/detail.js
@@ -1,44 +1,41 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {getPost} from "../../api/getApi";
 
-// 초기 상태값
 const initialState = {
     listData: [],
     listHeader: {},
-    loading: false
+    loading: false,
 };
 
 const detailSlice = createSlice({
     name: 'detail',
     initialState: initialState,
     reducers: {
-        loadingStart(state){
-            state.loading = true
+        loadingStart(state) {
+            state.loading = true;
         },
-        getListData(state, action){
-            state.listData = action.payload.data
-            state.listHeader = action.payload.headers
-            state.loading = false
+        getListData(state, action) {
+            state.listData = action.payload.data;
+            state.listHeader = { ...action.payload.headers }; // 직렬화 가능한 형태로 변환
+            state.loading = false;
         },
-        loadingFail(state){
-            state.loading = false
+        loadingFail(state) {
+            state.loading = false;
         }
     }
-})
+});
 
-export const detailActions = detailSlice.actions
+export const detailActions = detailSlice.actions;
 
 export const fetchListData = () => async (dispatch) => {
-        dispatch(detailActions.loadingStart())
+    dispatch(detailActions.loadingStart());
     try {
-        const response = await getPost()
-        dispatch(detailActions.getListData({data: response.data, headers: response.headers}))
-    }catch (error){
-            console.log(error,'에러뜸')
-        dispatch(detailActions.loadingFail())
+        const response = await getPost();
+        dispatch(detailActions.getListData({ data: response.data, headers: { ...response.headers } })); // 직렬화 가능한 형태로 변환
+    } catch (error) {
+        console.log(error, '에러뜸');
+        dispatch(detailActions.loadingFail());
     }
-}
-
-
+};
 
 export default detailSlice.reducer
